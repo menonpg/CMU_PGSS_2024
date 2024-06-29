94dd9dc753b02f611bd4ecf2576f3764c2298ca1
Prahlad Menon <menon.prahlad@gmail.com>
2024-06-29 08:23:53 -0400

    Update Lecture 2 (Week 1) content

---
+++ L2-6282024/on_start.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script runs every time your Studio starts, from your home directory.
+
+# List files under fast_load that need to load quickly on start (e.g. model checkpoints).
+#
+# ! fast_load
+# <your file here>
+
+# Add your startup commands below.
+#
+# Example: streamlit run my_app.py
+# Example: gradio my_app.py
+
+curl -fsSL https://ollama.com/install.sh | sh
+ollama run llava-phi3     
---
+++ L2-6282024/test.py
@@ -0,0 +1,10 @@
+def add_numbers(x, y):
+    return x + y
+
+if __name__ == '__main__':
+    # Test the function with some sample inputs
+    assert add_numbers(2, 3) == 5
+    assert add_numbers(-1, 4) == 3
+    print('All tests passed!')
+
+    print(add_numbers(10, 16))
